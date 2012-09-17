8220b3e293b3855193e78f626e693475dd4ea17b
choyno <ariel.lopezf@gmail.com>
2012-09-16 22:09:41 -0700

    changes

--- app/controllers/panel/request_substitutions_controller.rb
+++ app/controllers/panel/request_substitutions_controller.rb
@@ -1,15 +1,15 @@
 class Panel::RequestSubstitutionsController < ApplicationController
 
   def index
-    @request_substitutions = RequestSubstitution.all
+    @request_substitutions = Substitute.all
   end
   
   def new
-    @request_substitution = RequestSubstitution.new
+    @request_substitution = Substitute.new
   end
 
   def create
-    @request_substitution = RequestSubstitution.new(params[:request_substitution])
+    @request_substitution = Substitute.new(params[:request_substitution])
     
     if @requestsubstitution.save
       redirect_to panel_users_path, :notice => "User successfully created!"
