78098d3184d9a1f29b9752c50830eb9df251ad07
choyno <ariel.lopezf@gmail.com>
2012-09-19 10:40:22 -0700

    changes nav

--- app/controllers/request_substitutions_controller.rb
+++ app/controllers/request_substitutions_controller.rb
@@ -11,4 +11,19 @@ class RequestSubstitutionsController < ApplicationController
   def new
     @request_substitution = Substitute.new
   end
+
+
+  def create
+    @request_substitution = Substitute.new(params[:request_substitution])
+    
+    session[:request_substitution_id] = @request_substitution.id
+    
+    if @request_substitution.save
+      redirect_to users_path, :notice => "Request successfully created!"
+    else
+      render :new
+    end
+    
+  end
+
 end
