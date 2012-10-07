a61b6eedbed8b0d742c89ca4d08a4b6189e221db
choyno <ariel.lopezf@gmail.com>
2012-10-08 02:40:24 +0800

    changes

--- app/controllers/verify_requests_controller.rb
+++ app/controllers/verify_requests_controller.rb
@@ -1,7 +1,8 @@
 class VerifyRequestsController < ApplicationController
 
 	def index
-		@verify_requests = Substitute.all
+		@verify_requests = Substitute.status_is_substitute
+		                            .includes(:substitute_teacher, :teacher_subject)
 	end
 
 end
--- app/models/substitute.rb
+++ app/models/substitute.rb
@@ -1,13 +1,12 @@
 class Substitute < ActiveRecord::Base
 
   scope :status_is_substitute, where('statuses.name = ?', 'Substitute').includes(:status)
+  scope :status_is_vouch, where('statuses.name = ?', 'Vouch').includes(:status)
  
   belongs_to :substitute_teacher, class_name: :Teacher, foreign_key: :substitute_teacher_id
   belongs_to :teacher_subject
   belongs_to :status
-  belongs_to :reason
-  
-  
+  belongs_to :reason_teacher, class_name: :Reason,foreign_key: :reasons_id
 
   
 end
