dd6781a6ba205e8b270e22193762452acbc260aa
choyno <ariel.lopezf@gmail.com>
2012-10-09 04:37:40 +0800

    huhau labad

--- app/controllers/vouch_requests_controller.rb
+++ app/controllers/vouch_requests_controller.rb
@@ -4,5 +4,9 @@ class VouchRequestsController < ApplicationController
 		@vouch_requests = Substitute.status_is_vouch
 		                            .includes(:substitute_teacher, :teacher_subject)
 	end
+
+	def show
+		@vouch_request = Substitute.find(params[:id])
+	end
 	
 end
