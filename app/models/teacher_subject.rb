afc733860d1ae226dea6dfc827ce694cc998cdc8
Paul Lopez <cliche2004@yahoo.com>
2012-10-11 18:30:47 +0800

    with more search

--- app/models/teacher_subject.rb
+++ app/models/teacher_subject.rb
@@ -3,6 +3,11 @@ class TeacherSubject < ActiveRecord::Base
 
   scope :search_by_name, lambda { | value | teachers = Teacher.select('id').where("CONCAT(firstname, lastname) like ? ", "%#{value}%" )
                           self.where("teacher_id IN (?)", teachers.pluck(:id))}
+  
+  scope :search_by_subject, lambda { |value|
+                                subjects = Subject.where('name LIKE :value OR code :value', value: "%#{value}%")
+                                self.where('subject_id IN (?)', subjects.pluck(:id) )
+                            }
 
   scope :default_include, includes(:day_code, :teacher, :subject, :room, :section)
   
@@ -38,15 +43,27 @@ class TeacherSubject < ActiveRecord::Base
     self.time_end - self.time_start
   end
 
-  def self.search(search)
-
+  def self.search(search_by, search)
+    
     teacher_subject_scope = self.scoped({})
 
-    teacher_subject_scope = teacher_subject_scope.search_by_name(search) if search.present?
- 
+    case search_by
+    when 'Teacher Name'
+      teacher_subject_scope = teacher_subject_scope.search_by_name(search)
+    when 'Teacher Subject'
+      teacher_subject_scope = teacher_subject_scope.search_by_subject(search)
+    end
+    
     return teacher_subject_scope
 
   end
+  
+  def self.fetch_available_teacher(time_start, time_end)
+    # get teacher with the same sched
+    in_sched_teachers = self.where("time_start >= time(?) AND time_end <= time(?)", time_start, time_end)
+    
+  end
+  
 
 
 end
