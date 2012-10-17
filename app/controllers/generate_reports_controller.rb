e4300320bf8720445f5b9c240cf737134ad3a079
Paul Lopez <cliche2004@yahoo.com>
2012-10-17 12:30:05 +0800

    g

--- app/controllers/generate_reports_controller.rb
+++ app/controllers/generate_reports_controller.rb
@@ -1,10 +1,9 @@
 class GenerateReportsController < ApplicationController
 
 	def index
-		if params[:generate_by].strip == 'Substitute Teachers'
+		if params[:generate_by] && params[:generate_by].strip == 'Substitute Teachers'
 			@generate_reports = SubstituteReport.substitute_teacher(params[:start_date], params[:end_date])
 		else
-			#@generate_reports = Substitute.generate_absent_teacher_report(params[:start_date], params[:end_date])
 			@generate_reports = SubstituteReport.absent_teacher(params[:start_date], params[:end_date])
 		end
 	end
