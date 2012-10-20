e3d6d73d4a8003d977908b66ac2caeb1da0333b3
choyno <ariel.lopezf@gmail.com>
2012-10-20 20:58:10 +0800

    changes

--- app/models/substitute.rb
+++ app/models/substitute.rb
@@ -9,7 +9,7 @@ class Substitute < ActiveRecord::Base
   scope :request_type_unplanned, where( planned: false)
 
   scope :remind_by_date, where( 'request_at_to <= ?', Date.current + 1.day )
-  scope :count_remind_by_date, where( 'request_at_from <= ?', Date.current + 1.day )
+  scope :count_remind_by_date, where( 'request_at_to <= ?', Date.current + 1.day )
 
   scope :search_by_requested, lambda { | value | teachers = TeacherSubject.select('id').where("CONCAT(firstname, lastname) like ? ", "%#{value}%" )
                           self.where("teacher_subject_id IN (?)", teachers.pluck(:id))}
@@ -109,7 +109,7 @@ class Substitute < ActiveRecord::Base
                    } 
       end
     end
-    
+
     return results
   end
 
